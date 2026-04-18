ddd18d4de57a072bf982f2f7f2813e064832b4d8
gourav <gouravbesh@gmail.com>
2026-04-18 21:45:27 +0530

    add resume card to homepge

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,10 +16,21 @@ export default function Home() {
       <main className="bg-[url('/images/bg-main.svg')]">
       <Navbar/>
         <section className="main-section">
-          <div className="page-heading">
+          <div className="page-heading py-16">
             <h1>Track Your Applications & Resume Ratings</h1>
             <h2>Review your submissions and check AI-powered feedback.</h2>
           </div>
+
+          
+          {resumes.length > 0 ? (
+            <div className="resumes-section">
+              {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))}
+            </div>
+          ) : (
+            <p className="no-resumes">No resumes submitted yet. Start by uploading your resume!</p>
+          )}
         </section>
       </main>;
     </>
