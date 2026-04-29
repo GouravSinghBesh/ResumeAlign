1dbfec676dc1d5a3b6cd0dbc60b866079122ac36
gourav <gouravbesh@gmail.com>
2026-04-29 13:23:09 +0530

    resume feedback components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -66,7 +66,7 @@ const resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
-                            <ATS />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}/>
                             <Details feedback={feedback} />
                         </div>
                     ) : (
