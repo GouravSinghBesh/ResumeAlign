437c3df6483c68891b494cdcdf0d3f2afb5f77b5
gourav <gouravbesh@gmail.com>
2026-04-30 23:06:16 +0530

    Add delete all and delete individual button on resume card

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -4,7 +4,7 @@ import ScoreCircle from './ScoreCircle'
 import { usePuterStore } from '~/lib/puter';
 
 
-const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }, handleDelete }: { resume: Resume, handleDelete: (id: string) => void }) => {
     const [resumeUrl, setResumeUrl] = useState<string>('');
     const { fs } = usePuterStore();
 
@@ -21,30 +21,36 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
     }, [imagePath])
 
     return (
-        <Link to={`/resumes/${id}`} className='resume-card animate-in fade-in duration-1000'>
-            <div className='resume-card-header'>
-                <div className='flex flex-col gap-2'>
-                    {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
-                    {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
-                    {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
-                </div>
-                <div className='shrink-0'>
-                    <ScoreCircle score={feedback.overallScore} />
-                </div>
+        <>
+            <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000 group relative z-10'>
+            <div className='cursor-pointer absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity z-50' onClick={(e) => { e.preventDefault(); e.stopPropagation(); handleDelete(id); }}>
+                <img src='/icons/cross.svg' alt='remove' className='h-4 w-4' />
             </div>
-            {resumeUrl && (
-                <div className="gradient-border animate-in fade-in duration-1000">
-                    <div className="w-full h-full">
-                        <img
-                            src={resumeUrl}
-                            alt="resume"
-                            className="w-full h-[350px] max-sm:h-50 object-cover object-top"
-                        />
+                <div className='resume-card-header'>
+                    <div className='flex flex-col gap-2'>
+                        {companyName && <h2 className="text-black! font-bold wrap-break-words">{companyName}</h2>}
+                        {jobTitle && <h3 className="text-lg wrap-break-words text-gray-500">{jobTitle}</h3>}
+                        {!companyName && !jobTitle && <h2 className="text-black! font-bold">Resume</h2>}
+                    </div>
+                    <div className='shrink-0'>
+                        <ScoreCircle score={feedback.overallScore} />
                     </div>
                 </div>
-            )}
+                {resumeUrl && (
+                    <div className="gradient-border animate-in fade-in duration-1000">
+                        <div className="w-full h-full">
+                            <img
+                                src={resumeUrl}
+                                alt="resume"
+                                className="w-full h-87.5 max-sm:h-50 object-cover object-top"
+                            />
+                        </div>
+                    </div>
+                )}
 
-        </Link>
+            </Link>
+        </>
+
     )
 }
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,22 +16,43 @@ export function meta({ }: Route.MetaArgs) {
 export default function Home() {
   const [resumes, setResumes] = useState<Resume[]>([]);
   const [loadingResumes, setLoadingResumes] = useState(false);
-  const { kv } = usePuterStore();
+  const { kv, fs } = usePuterStore();
+  const [files, setFiles] = useState<FSItem[]>([]);
+
+  const handleDeleteAll = async () => {
+    // Delete all files in the file system
+    files.forEach(async (file) => {
+      await fs.delete(file.path);
+    })
+    await kv.flush();
+    loadFiles();
+    loadResumes();
+  }
+  const loadFiles = async () => {
+    const files = await fs.readDir("./") as FSItem[];
+    setFiles(files);
+  }
+  const loadResumes = async () => {
+    setLoadingResumes(true);
+
+    const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+    const parsedResumes = resumes.map(resume => (
+      JSON.parse(resume.value) as Resume
+    ))
+    setResumes(parsedResumes);
+    setLoadingResumes(false);
+  }
+  const handleDeleteResume = async (id: string) => {
+    await kv.delete(`resume:${id}`);
+    loadResumes();
+  }
 
   useEffect(() => {
-    const loadResumes = async () => {
-      setLoadingResumes(true);
-
-      const resumes = (await kv.list('resume:*', true)) as KVItem[];
-
-      const parsedResumes = resumes.map(resume => (
-        JSON.parse(resume.value) as Resume
-      ))
-      setResumes(parsedResumes);
-      setLoadingResumes(false);
-    }
     loadResumes();
+    loadFiles();
   }, [])
+
 
   return (
     <>
@@ -53,18 +74,28 @@ export default function Home() {
           {!loadingResumes && resumes?.length > 0 && (
             <div className="resumes-section">
               {resumes.map((resume) => (
-                <ResumeCard key={resume.id} resume={resume} />
+                <ResumeCard key={resume.id} resume={resume} handleDelete={handleDeleteResume} />
               ))}
             </div>
           )}
 
           {!loadingResumes && resumes?.length === 0 && (
             <div className="flex flex-col items-center justify-center mt-10 gap-4">
-            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
-              Upload Resume
-            </Link>
-          </div>
+              <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                Upload Resume
+              </Link>
+            </div>
           )}
+          {!loadingResumes && resumes?.length > 0 && (
+            <div>
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                onClick={handleDeleteAll}>
+                Clear all resumes and files
+              </button>
+            </div>
+          )}
+
         </section>
       </main>;
     </>
